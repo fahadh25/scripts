9c38a43dfed9939a9afc8924d75ea02dee30cc63
fahadh25 <57483996+fahadh25@users.noreply.github.com>
2022-05-18 03:32:26 +0600

    Update bandwidth_check.sh

--- bandwidth_check.sh
+++ bandwidth_check.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+#This command will check the bandwidth consumption.
+#Please install the package first if it's not available 
+#configure this as cron for recurring report
 vnstat -l -i em1
