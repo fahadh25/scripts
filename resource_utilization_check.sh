74e9a49507ca147bcdefe58112b2c5b49f4435da
fahadh25 <57483996+fahadh25@users.noreply.github.com>
2022-05-18 03:39:39 +0600

    Update resource_utilization_check.sh

--- resource_utilization_check.sh
+++ resource_utilization_check.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+#This script will check the RAM, CPU and Disk consumption
+#and trigger a mail alert if utilization cross threshold.
+
+#Vairable call for email alert
 IP=$(hostname -I | awk '{print $1}')
-TO=git-bd-alerts@augmedix.com
+TO=your email address
 HOST=$HOSTNAME
 
 #Variable call for Disk space
 DISK=$(df -h --total | awk '$1=="total"{printf $5}' | cut -d '%' -f1)
 THRESHOLD=70
 
-#Check Disk space
+#Check Disk space and trigger email 
 if (( $(echo "$DISK > $THRESHOLD" | bc -l) )); then
- mail -r "Resource Alert <it.escalation@gmail.com>" -s " Resource limitation alert from $IP | $HOST " $TO,  << EOF
+ mail -r "Resource Alert <from_email@example.com>" -s " Resource limitation alert from $IP | $HOST " $TO,  << EOF
  Disk space of below server is critically low.
  IP: $IP
  HOSTNAME: $HOST
@@ -23,9 +27,9 @@ fi
 RAM=$(free -m | awk 'NR==2{printf "%.2f\n", $3*100/$2 }')
 THRESHOLD=80
 
-#Check RAM consumption
+#Check RAM consumption and trigger email 
 if (( $(echo "$RAM > $THRESHOLD" | bc -l) )); then
- mail -r "Resource Alert <it.escalation@gmail.com>" -s " Resource limitation alert from $IP | $HOST "  $TO,  << EOF
+ mail -r "Resource Alert <from_email@example.com>" -s " Resource limitation alert from $IP | $HOST "  $TO,  << EOF
  RAM consumption of below server is very high.
  IP: $IP
  HOSTNAME: $HOST
@@ -38,9 +42,9 @@ fi
 CPU=$(top -bn1 | grep load | awk '{printf "%.2f\n", $(NF-2)}')
 THRESHOLD=70
 
-#Check CPU load
+#Check CPU load and trigger email 
  if (( $(echo "$CPU > $THRESHOLD" | bc -l) )); then
- mail -r "Resource Alert <it.escalation@gmail.com>" -s " Resource limitation alert from $IP | $HOST "  $TO,  << EOF
+ mail -r "Resource Alert <from_email@example.com>" -s " Resource limitation alert from $IP | $HOST "  $TO,  << EOF
  CPU load average of below server is very high.
  IP: $IP
  HOSTNAME: $HOST 
